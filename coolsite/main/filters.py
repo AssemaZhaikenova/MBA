39c8b8ff49d8d040381d51629a335716e65ab820
Assem <123903846+AssemaZhaikenova@users.noreply.github.com>
2023-02-03 03:41:07 +0600

    Add files via upload

---
+++ coolsite/main/filters.py
@@ -0,0 +1,9 @@
+import django_filters
+
+from .models import Lesson
+
+class LessonFilter(django_filters.FilterSet):
+
+    class Meta:
+        model = Lesson
+        fields = ['discipline', 'city', 'teacher']
---
+++ coolsite/main/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='main-index'),
+    path('about/', views.about, name='main-about'),
+    path('login/', views.login_v, name='main-login'),
+    path('lesson/', views.lesson, name='main-lesson'),
+    path('mylessons/', views.mylesson, name='main-mylessons'),
+    path('logout/', views.logout_view, name='main-logout'),
+    path('requests/', views.requests, name='main-requests'),
+    path('addrequest/', views.select_view, name='main-addrequest'),
+    path('coach/', views.coach, name='main-coach'),
+    path('disciplines/', views.disciplines, name='main-disciplines'),
+
+]
